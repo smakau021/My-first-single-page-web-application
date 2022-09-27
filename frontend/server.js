ce84a1139ce1c3e269b7237f1b6738d23baab261
Sphelele Makau <smakau021@student.wethinkcode.co.za>
2022-09-27 15:41:39 +0200

    I am not yet able to start the server with command : node server.js so I run the program with no debug---We getting there!

--- frontend/server.js
+++ frontend/server.js
@@ -6,4 +6,8 @@ const app = express();
 "Here I am basically saying any route / path at all, we gonna gonna go back to the root and send back our index html"
 
 app.get("/*", (req, res) =>
-    res.sendFile(path.resolve("frontend", "index.html")));
+    res.sendFile(path.resolve("/home/sphe/SecondSementer/My-first-single-page-web-application/frontend", "index.html")));
+"No matter what path I send to the web server to route to any route , they all gonna go back to this index.html"
+    
+
+app.listen(process.emp || 5050, () => console.log("Server running ......"))
